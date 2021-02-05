d65ba89a71d157ff91a39fdb60534428b9396c03
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-02-05 01:17:19 +0100

    Comments added

--- tg.py
+++ tg.py
@@ -1,13 +1,11 @@
 from telegram import InlineKeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup
-# import logging
 
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-# )
 
-# logger = logging.getLogger(__name__)
-
-def buildKeyboard(buttons, back=False, state='Back', one=True):
+# Builds a keyboard from a list buttons.
+# If back True, adds a back button.
+# one determines if it is for one time
+# It displays the buttons in two columns. It uses only one column if the text is too long
+def buildKeyboard(buttons, back=False, one=True):
     keys = []
     index = 0
     for button in buttons:
@@ -26,6 +24,12 @@ def buildKeyboard(buttons, back=False, state='Back', one=True):
     keyboard = ReplyKeyboardMarkup(keys, one_time_keyboard=mode)
     return keyboard
 
+# Builds an inline keyboard from a list buttons. The callback data will be the same as the buttons name. 
+# If back True, adds a back button.
+# state determines the value of the callback data of the back button. 
+# Marker adds the specified string to the beggining of the callback data.
+# It displays the buttons in two columns. It uses only one column if the text is too long.
+# It also cuts the callback data to the first 45 characters, to avoid limit restrictions
 def buildInline(buttons, back=False, state='Back', marker=''):
     keyboard = []
     index = 0
@@ -43,6 +47,7 @@ def buildInline(buttons, back=False, state='Back', marker=''):
         keyboard.append([InlineKeyboardButton('Back', callback_data=state)])
     return InlineKeyboardMarkup(keyboard)
 
+# Returns the username. In case it doesn't exist, it returns the first name with the last name (if any)
 def get_user(update):
     if update.callback_query:
         data = update.callback_query.message.chat
